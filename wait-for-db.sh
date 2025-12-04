b9df90a84b9bde674603a8fb28a000bc5d7213f9
Damian Zelinka <damzel321@gmail.com>
2025-12-04 16:16:03 +0300

    wait

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
-# wait-for-db.sh
-
-set -e
 
 host="$1"
-shift
-cmd="$@"
+port="$2"
+shift 2
 
-echo "Waiting for PostgreSQL at $host..."
+echo "Waiting for PostgreSQL at $host:$port..."
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
+# Use DB credentials from env or defaults
+: "${POSTGRES_USER:=damko}"
+: "${POSTGRES_PASSWORD:=damko}"
+: "${POSTGRES_DB:=lab2}"
+
+export PGPASSWORD="$POSTGRES_PASSWORD"
+
+until psql -h "$host" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -p "$port" -c '\q' >/dev/null 2>&1; do
   echo "PostgreSQL is unavailable - sleeping"
-  sleep 2
+  sleep 1
 done
 
 echo "PostgreSQL is up - executing command"
-exec $cmd
+exec "$@"
