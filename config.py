ccb1f3691a934a8d6005f2bafeef3331604b93dc
Damian-Zelinka <146333723+Damian-Zelinka@users.noreply.github.com>
2025-12-04 13:01:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:furry@localhost/bee_pro'
+SQLALCHEMY_DATABASE_URI = 'postgresql://damko:damko@bee_db:5432/lab2'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = '/RiXnoGXTLXVyfanm2XgKA=='
