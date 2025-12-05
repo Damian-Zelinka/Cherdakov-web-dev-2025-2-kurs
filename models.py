b7a5025bd67ff854f9e35569db977158e8e5ceb3
Damian Zelinka <damzel321@gmail.com>
2025-12-05 09:19:10 +0300

    updated models

--- models.py
+++ models.py
@@ -87,6 +87,21 @@ class BeeCoinTransaction(db.Model):
     description = db.Column(db.String(255))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
+
+
+
+
+class NewTestTable(db.model):
+    __tablename__ = 'new_test_table'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    year = db.Column(db.Numeric(10, 2), nullable=False)
+    type = db.Column(db.String(20), nullable=False)
+    model= db.Column(db.String(255))
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+
 # Индексы
 db.Index('idx_users_email', User.email)
 db.Index('idx_orders_user', Order.user_id)
